50e81c6d0a657220af67a1ed58a74f1bb6765a42
Your Name <you@example.com>
2020-11-10 13:56:56 +0200

    M

--- src/cmps/Enemy.jsx
+++ src/cmps/Enemy.jsx
@@ -4,16 +4,14 @@ import { GoTriangleDown } from 'react-icons/go'
 export function Enemy({ char, onSelect }) {
     return (
         <div className='enemy char-container relative'
-            style={{ marginRight: char.marginRight, alignSelf: char.alignSelf }}
+            style={{ marginRight: char.marginRight, alignSelf: char.alignSelf , opacity:`${char.currHp<=0 ? 0 : 1}`}}
             onClick={onSelect}>
             <img className="enemy-sprite" src={char.gif} alt="" />
             <div className="absolute">{char.currHp>=0 ? char.currHp : 0}/{char.hp}</div>
             { char.dmgDoneTo.isShown &&
                 <div className="dmg-output absolute"
                     style={{ color: char.dmgDoneTo.isCrit ? 'red' : 'green', fontSize: char.dmgDoneTo.isCrit ? '40px' : '25px' }}>
-
                     {char.dmgDoneTo.amount}
-
                 </div>}
 
             { char.isSelected && <GoTriangleDown className="absolute" />}
--- src/cmps/Player.jsx
+++ src/cmps/Player.jsx
@@ -2,12 +2,29 @@ import React from 'react'
 
 export function Player({ char }) {
     return (
-        <div className='player char-container relative' style={{ marginLeft: char.marginLeft, alignSelf: char.alignSelf }}>
+        <div className='player char-container relative' style={{ marginLeft: char.marginLeft, alignSelf: char.alignSelf ,transition: char.transition}}>
             <img className="player-sprite" src={char.gif} alt="" />
-            <div className="absolute">{char.currHp}/{char.hp}</div>
+            <div className="hp absolute" style={{
+                height:'10px',
+                width: char.hp+'px',
+                backgroundColor:'black'
+            }}>
+                {char.currHp<=0 ? 0 : char.currHp}/{char.hp}
+            <div className="curr-hp" style={{
+                height:'100%',
+                width: char.currHp+'px',
+                backgroundColor:'red'
+            }}>
+
+            </div>
+            </div>
             { char.dmgDoneTo.isShown &&
                 <div className="dmg-output absolute"
-                    style={{ color: char.dmgDoneTo.isCrit ? 'red' : 'green', fontSize: char.dmgDoneTo.isCrit ? '40px' : '25px' }}>
+                    style={{ color: char.dmgDoneTo.isCrit ? 'red' : 'green',
+                     fontSize: char.dmgDoneTo.isCrit ? '40px' : '25px',
+                     opacity: char.currHp<=0 ? 0 : 1,
+                     transition:'.5s'
+                    }}>
 
                     {char.dmgDoneTo.amount}
 
