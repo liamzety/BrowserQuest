bbd262874b1c11293ebf59043ce0d9f2a483ec25
Your Name <you@example.com>
2020-11-10 14:16:03 +0200

    M

--- src/cmps/Enemy.jsx
+++ src/cmps/Enemy.jsx
@@ -7,7 +7,23 @@ export function Enemy({ char, onSelect }) {
             style={{ marginRight: char.marginRight, alignSelf: char.alignSelf , opacity:`${char.currHp<=0 ? 0 : 1}`}}
             onClick={onSelect}>
             <img className="enemy-sprite" src={char.gif} alt="" />
-            <div className="absolute">{char.currHp>=0 ? char.currHp : 0}/{char.hp}</div>
+            <div className="relative flex align-center justify-center">
+
+            <div className="hp" style={{
+                height: '20px',
+                width: char.hp + 'px',
+                backgroundColor: 'black',
+                left: '70px'
+            }}>
+                <div className="curr-hp" style={{
+                    height: '100%',
+                    width: char.currHp + 'px',
+                    backgroundColor: 'red'
+                }}>
+                </div>
+            </div>
+            <p class="absolute" style={{width:char.hp+'px', textAlign:'center'}}>{char.currHp <= 0 ? 0 : char.currHp}/{char.hp}</p> 
+            </div>
             { char.dmgDoneTo.isShown &&
                 <div className="dmg-output absolute"
                     style={{ color: char.dmgDoneTo.isCrit ? 'red' : 'green', fontSize: char.dmgDoneTo.isCrit ? '40px' : '25px' }}>
--- src/cmps/Player.jsx
+++ src/cmps/Player.jsx
@@ -2,28 +2,30 @@ import React from 'react'
 
 export function Player({ char }) {
     return (
-        <div className='player char-container relative' style={{ marginLeft: char.marginLeft, alignSelf: char.alignSelf ,transition: char.transition}}>
+        <div className='player char-container relative' style={{ marginLeft: char.marginLeft, alignSelf: char.alignSelf, transition: char.transition }}>
             <img className="player-sprite" src={char.gif} alt="" />
             <div className="hp absolute" style={{
-                height:'10px',
-                width: char.hp+'px',
-                backgroundColor:'black'
-            }}>
-                {char.currHp<=0 ? 0 : char.currHp}/{char.hp}
-            <div className="curr-hp" style={{
-                height:'100%',
-                width: char.currHp+'px',
-                backgroundColor:'red'
+                height: '20px',
+                width: char.hp + 'px',
+                backgroundColor: 'black',
+                left: '70px'
             }}>
 
-            </div>
+                <div className="curr-hp absolute flex align-center" style={{
+                    height: '100%',
+                    width: char.currHp + 'px',
+                    backgroundColor: 'red'
+                }}>
+                   <p style={{width:char.hp+'px', textAlign:'center'}}>{char.currHp <= 0 ? 0 : char.currHp}/{char.hp}</p> 
+                </div>
             </div>
             { char.dmgDoneTo.isShown &&
                 <div className="dmg-output absolute"
-                    style={{ color: char.dmgDoneTo.isCrit ? 'red' : 'green',
-                     fontSize: char.dmgDoneTo.isCrit ? '40px' : '25px',
-                     opacity: char.currHp<=0 ? 0 : 1,
-                     transition:'.5s'
+                    style={{
+                        color: char.dmgDoneTo.isCrit ? 'red' : 'green',
+                        fontSize: char.dmgDoneTo.isCrit ? '40px' : '25px',
+                        opacity: char.currHp <= 0 ? 0 : 1,
+                        transition: '.5s'
                     }}>
 
                     {char.dmgDoneTo.amount}
