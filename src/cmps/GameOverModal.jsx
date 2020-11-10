1174dde441546cb60214261ec845bb9011ff3c62
liamzety <58632095+liamzety@users.noreply.github.com>
2020-11-10 13:57:08 +0200

    M

--- src/cmps/GameOverModal.jsx
+++ src/cmps/GameOverModal.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import cursor from '../assets/images/cursor.png'
 
 export function GameOverModal() {
+    const [gauntlet, setGauntlet] = useState({
+        g1: false,
+        g2: false,
+        g3: false
+    })
     return (
-        <div className="game-over-modal fixed">
+        <>
 
-        </div>
+            <div className="game-over-modal fixed">
+                <div className="game-over-modal-container space-between flex align-center col h100 ">
+                    <h1>GAME OVER</h1>
+                    <div className="game-actions-container flex col w100">
+                        <div className="btn-container">
+                            <img className="cursor" style={{ opacity: gauntlet.g1 ? '1' : '0' }} src={cursor} alt="" />
+                            <button onMouseEnter={() => { setGauntlet(prevState => { return { ...prevState, g1: true } }) }}
+                                onMouseLeave={() => { setGauntlet(prevState => { return { ...prevState, g1: false } }) }}>
+                                <span>R</span>estart</button>
+                        </div>
+                        <div className="btn-container">
+                            <img className="cursor" style={{ opacity: gauntlet.g2 ? '1' : '0' }} src={cursor} alt="" />
+                            <button onMouseEnter={() => { setGauntlet(prevState => { return { ...prevState, g2: true } }) }}
+                                onMouseLeave={() => { setGauntlet(prevState => { return { ...prevState, g2: false } }) }}>
+                                <span>C</span>haracter Select</button>
+                        </div>
+                        <div className="btn-container">
+                            <img className="cursor" style={{ opacity: gauntlet.g3 ? '1' : '0' }} src={cursor} alt="" />
+                            <button onMouseEnter={() => { setGauntlet(prevState => { return { ...prevState, g3: true } }) }}
+                                onMouseLeave={() => { setGauntlet(prevState => { return { ...prevState, g3: false } }) }}>
+                                <span>A</span>bout</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className="modal-screen-wrapper"></div>
+        </>
     )
 }
